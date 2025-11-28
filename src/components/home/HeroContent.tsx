ee0c7d5dbac3a220af00712dd2ae98eb2190fa21
nguyen cong trung <congtrung1004@gmail.com>
2025-11-28 14:07:11 +0700

    1

--- src/components/home/HeroContent.tsx
+++ src/components/home/HeroContent.tsx
@@ -5,71 +5,72 @@ import Image from 'next/image';
 import appStore from '../../../public/images/home/app-store.png';
 import googlePlay from '../../../public/images/home/google-play.png';
 import line from '../../../public/images/home/line.png';
-import frame from '../../../public/images/home/logo2.png';
 import porcher from '../../../public/images/home/porcher.png';
 
 const HeroContent = () => {
   return (
-    <div className='relative z-20 w-full'>
-      <div className='grid grid-cols-1 lg:grid-cols-[1fr_1.2fr] gap-0 items-center max-w-[1600px] mx-auto'>
-        
-        {/* Left Content - Dịch lên trên và qua phải */}
-        <div className='flex flex-col text-center lg:text-left items-center lg:items-start order-2 lg:order-1 lg:pl-50 lg:-mt-33 px-4 lg:gap-1'>
-          <h1 className="text-3xl md:text-4xl lg:text-[48px] leading-[46px]  font-medium text-gray-900 ">
-            Tìm, đặt và
-            <br />
-            thuê xe một cách
-            <br />
-            <span className='inline-flex flex-col text-[#1572D3] w-max mt-0 '>
-              Dễ dàng
+    <div className='relative z-20 w-full px-4 lg:px-0'>
+      <div className='max-w-[1600px] mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-0 items-center'>
+          
+          {/* Left Content - Text Section */}
+          <div className='flex flex-col text-center lg:text-left items-center lg:items-start order-2 lg:order-1 lg:pl-16 xl:pl-24 2xl:pl-32'>
+            <h1 className="text-3xl md:text-4xl xl:text-5xl 2xl:text-[48px] leading-tight font-medium text-gray-900 mb-4">
+              Tìm, đặt và
+              <br />
+              thuê xe một cách
+              <br />
+              <span className='inline-flex flex-col text-[#1572D3] w-max mt-1'>
+                Dễ dàng
+                <Image 
+                  src={line} 
+                  alt="line decoration" 
+                  className='self-end -mt-1 mr-2' 
+                  width={130} 
+                  height={10} 
+                />
+              </span>
+            </h1>
+            
+            <p className="text-sm md:text-base lg:text-base xl:text-lg text-gray-600 max-w-md font-medium">
+              Đặt xe ở <span className="text-[#0050B3]">bất cứ đâu, bất cứ khi nào</span> chỉ với thiết bị iOS hoặc Android của bạn!
+            </p>
+            
+            {/* App Store Buttons */}
+            <div className='flex flex-col sm:flex-row gap-4 mt-6 w-full sm:w-auto'>
               <Image 
-                src={line} 
-                alt="line decoration" 
-                className='self-end -mt-0.5  mr-2 ' 
+                src={appStore} 
+                alt="Download on App Store" 
                 width={130} 
-                height={10} 
+                height={40} 
+                className="cursor-pointer hover:opacity-80 transition-opacity mx-auto sm:mx-0" 
               />
-            </span>
-          </h1>
-          
-          <p className="text-sm md:text-base text-gray-600 max-w-md mt-4  font-medium lg:text-[16px]">
-            Đặt xe ở <span className="text-[#0050B3] ">bất cứ đâu, bất cứ khi nào</span> chỉ với thiết bị iOS hoặc Android của bạn!
-          </p>
-          
-          {/* App Store Buttons */}
-          <div className='flex flex-col sm:flex-row gap-4 mt-6 w-full sm:w-auto'>
-            <Image 
-              src={appStore} 
-              alt="Download on App Store" 
-              width={130} 
-              height={40} 
-              className="cursor-pointer hover:opacity-80 transition-opacity mx-auto sm:mx-0" 
-            />
-            <Image 
-              src={googlePlay} 
-              alt="Get it on Google Play" 
-              width={130} 
-              height={40} 
-              className="cursor-pointer hover:opacity-80 transition-opacity mx-auto sm:mx-0" 
-            />
+              <Image 
+                src={googlePlay} 
+                alt="Get it on Google Play" 
+                width={130} 
+                height={40} 
+                className="cursor-pointer hover:opacity-80 transition-opacity mx-auto sm:mx-0" 
+              />
+            </div>
           </div>
-        </div>
-        
-        {/* Right Content - Car Image sát bên phải và giữ kích thước lớn */}
-        <div className='relative flex justify-center lg:justify-end items-center order-1 lg:order-2 min-h-[300px] lg:min-h-[500px] lg:pr-0 lg:mt-15'>
-          {/* Car Image - Giữ kích thước gốc */}
-          <div className='relative z-10 w-full lg:w-[850px]'>
-            <Image 
-              src={porcher}
-              alt='Porsche car'
-              width={1048}   
-              height={537}  
-              priority 
-              className='object-contain drop-shadow-2xl w-full h-auto'
-            />
+          
+          {/* Right Content - Car Image */}
+          <div className='relative flex justify-center lg:justify-end items-center order-1 lg:order-2 min-h-[300px] lg:min-h-[450px] xl:min-h-[500px]'>
+            {/* Car Image - Responsive sizing */}
+            <div className='relative z-10 w-full max-w-[500px] lg:max-w-[650px] xl:max-w-[750px] 2xl:max-w-[850px]'>
+              <Image 
+                src={porcher}
+                alt='Porsche car'
+                width={1048}   
+                height={537}  
+                priority 
+                className='object-contain drop-shadow-2xl w-full h-auto'
+              />
+            </div>
           </div>
+          
         </div>
-        
       </div>
     </div>
   );
--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import Header from '@/components/layout/Header';
 import SearchBox from '@/components/home/SearchBox';
 import HeroContent from '@/components/home/HeroContent';
-import FloatingActionButton from '../common/FloatingActionButton';
 import Image from 'next/image';
 import logo2 from '../../../public/images/home/logo2.png'
+
 const HeroSection = () => {
   return (
-    <section className='w-full min-h-screen lg:h-[870px] relative overflow-hidden '>
+    <section className='w-full min-h-screen lg:h-[870px] relative overflow-hidden'>
       
-      <Image src={logo2} alt='logo2' className='absolute w-[800px] h-[850px] -top-2.5] left-[866px]  z-0'/>
+      {/* Background Logo - Fixed positioning for large screens */}
+      <Image 
+        src={logo2} 
+        alt='logo2' 
+        width={800}
+        height={850}
+        className='absolute w-[400px] h-[425px] lg:w-[800px] lg:h-[850px] -top-2 right-0 lg:left-[866px] z-0 opacity-50 lg:opacity-100'
+      />
+      
       {/* Header */}
       <div className='relative z-30 py-4 lg:py-10'>
         <Header />
@@ -18,12 +26,11 @@ const HeroSection = () => {
       {/* Hero Content */}
       <HeroContent />
       
-      {/* Search Box - Đè lên hình ảnh xe trên desktop */}
-      <div className='relative lg:absolute lg:bottom-21 lg:left-1/2 lg:-translate-x-1/2 z-30 mt-12 lg:mt-0 pb-12 lg:pb-0'>
+      {/* Search Box - Positioned at bottom on desktop */}
+      <div className='relative lg:absolute lg:bottom-20 lg:left-1/2 lg:-translate-x-1/2 z-30 mt-12 lg:mt-0 pb-12 lg:pb-0'>
         <SearchBox />
       </div>
       
-     
     </section>
   );
 };
